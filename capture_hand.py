544d643a20bfa0697a8b29a0c458b153b6702c08
WeiliLiu <weili_liu@berkeley.edu>
2020-10-29 22:29:22 -0700

    hand video processing

--- capture_hand.py
+++ capture_hand.py
@@ -6,11 +6,12 @@ import glob
 window_name = "Hand Capture"
 HSV_min_value = 0
 SV_max_value = 255
-H_max_value = 25
+H_max_value = 360
 gaussian_blur_min = 1
 gaussian_blur_max = 20
 morpho_kernel_min = 1
 morpho_kernel_max = 20
+max_binary_value = 255
 
 # This function records images from the connected camera to specified directory 
 # when the "Space" key is pressed.
@@ -21,16 +22,23 @@ def CaptureImages(directory):
     # the 0 value should default to the webcam, but you may need to change this
     # for your camera, especially if you are using a camera besides the default
     cam = cv2.VideoCapture(0)
+    cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25) # 0.25 turns off auto exp
+    cam.set(cv2.CAP_PROP_AUTO_WB, 0.25) # 0.25 turns off auto wb
     cv2.namedWindow(window_name)
 
     # Create Trackbar to choose values for HSV lower bound
-    cv2.createTrackbar('H', window_name, HSV_min_value, H_max_value, nothing)
-    cv2.createTrackbar('S', window_name, HSV_min_value, SV_max_value, nothing)
-    cv2.createTrackbar('V', window_name, HSV_min_value, SV_max_value, nothing)
+    cv2.createTrackbar('H_lo', window_name, 0, H_max_value, nothing)
+    cv2.createTrackbar('S_lo', window_name, 0, SV_max_value, nothing)
+    cv2.createTrackbar('V_lo', window_name, 0, SV_max_value, nothing)
+    cv2.createTrackbar('H_hi', window_name, 0, H_max_value, nothing)
+    cv2.createTrackbar('S_hi', window_name, 0, SV_max_value, nothing)
+    cv2.createTrackbar('V_hi', window_name, 0, SV_max_value, nothing)
     # Create Trackbar to choose value for morphological transforms kernel
-    cv2.createTrackbar('kernel', window_name, morpho_kernel_min, morpho_kernel_max, nothing)
+    # cv2.createTrackbar('kernel', window_name, morpho_kernel_min, morpho_kernel_max, nothing)
     # Create Trackbar to choose value for gaussian blur
-    cv2.createTrackbar('blur', window_name, gaussian_blur_min, gaussian_blur_max, nothing)
+    # cv2.createTrackbar('blur', window_name, gaussian_blur_min, gaussian_blur_max, nothing)
+    # Create Trackbar to choose value for threshold
+    cv2.createTrackbar('threshold', window_name, 0, 255, nothing)
 
     img_counter = 0
     # Read until user quits
@@ -40,10 +48,11 @@ def CaptureImages(directory):
             break
 
         # Extract current lower_HSV values
-        H_lo, S_lo, V_lo = cv2.getTrackbarPos('H', window_name), cv2.getTrackbarPos('S', window_name), cv2.getTrackbarPos('V', window_name)
+        H_lo, S_lo, V_lo = cv2.getTrackbarPos('H_lo', window_name), cv2.getTrackbarPos('S_lo', window_name), cv2.getTrackbarPos('V_lo', window_name)
+        H_hi, S_hi, V_hi = cv2.getTrackbarPos('H_hi', window_name), cv2.getTrackbarPos('S_hi', window_name), cv2.getTrackbarPos('V_hi', window_name)
 
-        lower_HSV = np.array([60, 100, 100], dtype="uint8")
-        upper_HSV = np.array([25, 255, 255], dtype="uint8")
+        lower_HSV = np.array([0, 44, 35], dtype="uint8")
+        upper_HSV = np.array([241, 255, 36], dtype="uint8")
 
         convertedHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         skinMaskHSV = cv2.inRange(convertedHSV, lower_HSV, upper_HSV)
@@ -56,21 +65,65 @@ def CaptureImages(directory):
 
         skinMask = cv2.add(skinMaskHSV, skinMaskYCrCb)
 
-        # # Apply morphological transforms and standard gaussian blur
-        # kernel_value = cv2.getTrackbarPos('kernel', window_name)
-        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
-        # skinMask = cv2.erode(skinMask, kernel, iterations = 2)
-        # skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+        # skinMask = skinMaskHSV
 
-        # # blur the mask to help remove noise, then apply the
-        # # mask to the frame
-        # blur_value = cv2.getTrackbarPos('blur', window_name)
-        # blur_value = blur_value + (blur_value % 2 == 0)
-        # skinMask = cv2.GaussianBlur(skinMask, (blur_value, blur_value), 0)
-        skin = cv2.bitwise_and(frame, frame, mask=skinMask)
+        # Apply morphological transforms and standard gaussian blur
+        # kernel_value = cv2.getTrackbarPos('kernel', window_name)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+        skinMask = cv2.erode(skinMask, kernel, iterations = 2)
+        skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
+
+        # blur the mask to help remove noise, then apply the
+        # mask to the frame
+        blur_value = cv2.getTrackbarPos('blur', window_name)
+        blur_value = blur_value + (blur_value % 2 == 0)
+        skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
+        frame = cv2.bitwise_and(frame, frame, mask=skinMask)
+
+        # Binarize the image
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        threshold_value = cv2.getTrackbarPos('threshold', window_name)
+        ret, thresh = cv2.threshold(gray, 0, threshold_value, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+        # Connected components analysis
+        ret, markers, stats, centroids = cv2.connectedComponentsWithStats(thresh, ltype=cv2.CV_16U)
+        markers = np.array(markers, dtype=np.uint8)
+        label_hue = np.uint8(179 * markers / np.max(markers))
+        blank_ch = 255 * np.ones_like(label_hue)
+        labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+        labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+        labeled_img[label_hue==0] = 0
+
+        if (ret > 2):
+            try:
+                statsSortedByArea = stats[np.argsort(stats[:, 4])]
+                roi = statsSortedByArea[-3][0:4]
+                x, y, w, h = roi
+                subImg = labeled_img[y:y+h, x:x+w]
+                subImg = cv2.cvtColor(subImg, cv2.COLOR_BGR2GRAY)
+                _, contours, _ = cv2.findContours(subImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                maxCntLength = 0
+                for i in range(0, len(contours)):
+                    cntLength = len(contours[i])
+                    if (cntLength > maxCntLength):
+                        cnt = contours[i]
+                        maxCntLength = cntLength
+                    if (maxCntLength >= 5):
+                        ellipseParam = cv2.fitEllipse(cnt)
+                        subImg = cv2.cvtColor(subImg, cv2.COLOR_GRAY2RGB)
+                        subImg = cv2.ellipse(subImg, ellipseParam, (0, 255, 0), 2)
+                    
+                    (x, y), (MA, ma), angle = cv2.fitEllipse(cnt)
+                    print("(x, y) = ({}, {}), (MA, ma) = ({}, {}), angle = {}".format(x, y, MA, ma, angle))
+
+                    subImg = cv2.resize(subImg, (0, 0), fx=3, fy=3)
+                    cv2.imshow("ROI "+str(2), subImg)
+                    cv2.waitKey(1)
+            except:
+                print("No hand found")
 
         # display the current image
-        cv2.imshow("Display", skin)
+        cv2.imshow("Display", labeled_img)
         # wait for 1ms or key press
         k = cv2.waitKey(1) #k is the key pressed
         if k == 27 or k == 113: #27, 113 are ascii for escape and q respectively
