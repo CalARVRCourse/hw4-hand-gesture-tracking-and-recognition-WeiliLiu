4f61cf4f70b0a6ee39e76164b67bc5bf2072d5c0
WeiliLiu <weili_liu@berkeley.edu>
2020-11-01 11:55:59 -0800

    temp push

--- capture_hand.py
+++ capture_hand.py
@@ -16,24 +16,32 @@ morpho_kernel_min = 1
 morpho_kernel_max = 20
 max_binary_value = 255
 # Camera Resolutions
+screen_width_res = 3840
+screen_height_res = 2160
 cam_width_res = 1280
 cam_height_res = 720
+# pyautogui constants
+offsetX = 0
+offsetY = 0
+scaleX = screen_width_res / cam_width_res
+scaleY = screen_height_res / cam_height_res
 
 # This function records images from the connected camera to specified directory 
 # when the "Space" key is pressed.
 # directory: should be a string corresponding to the name of an existing 
 # directory
-def CaptureImages(directory, part=1, tune=False, heuristic=False):
+def CaptureImages(directory, part=1, tune=False, heuristic=False, twa=False):
     # Open the camera for capture
     # the 0 value should default to the webcam, but you may need to change this
     # for your camera, especially if you are using a camera besides the default
-    cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(1)
     cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25) # 0.25 turns off auto exp
     cam.set(cv2.CAP_PROP_AUTO_WB, 0.25) # 0.25 turns off auto wb
     # Setting camera resolutions
     cam.set(cv2.CAP_PROP_FRAME_WIDTH, cam_width_res)
     cam.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_height_res)
-    cv2.namedWindow(window_name)
+    if tune:
+        cv2.namedWindow(window_name)
 
     # Create Trackbar to choose values for HSV lower bound
     cv2.createTrackbar('H_lo', window_name, 0, H_max_value, nothing)
@@ -50,12 +58,12 @@ def CaptureImages(directory, part=1, tune=False, heuristic=False):
     cv2.createTrackbar('threshold', window_name, 0, 255, nothing)
 
     # Variables for decreasing opencv noise
-    last_three_frames = []
     r_vals = []
     g_vals = []
     b_vals = []
     (rAvg, gAvg, bAvg) = (None, None, None)
     total = 0
+    angleAvg = None
 
     img_counter = 0
     # Read until user quits
@@ -64,18 +72,21 @@ def CaptureImages(directory, part=1, tune=False, heuristic=False):
         if not ret:
             break
         
-        (B, G, R) = cv2.split(frame.astype("float"))
-        if rAvg is None or total == 3:
-            rAvg, gAvg, bAvg = R, G, B
-            total = 0
-        else:
-            rAvg = ((total * rAvg) + (1 * R)) / (total + 1.0)
-            gAvg = ((total * gAvg) + (1 * G)) / (total + 1.0)
-            bAvg = ((total * bAvg) + (1 * B)) / (total + 1.0)
-        total += 1
-        frame = cv2.merge([bAvg, gAvg, rAvg]).astype("uint8")
-        print(np.shape(frame))
+        if twa:
+            (B, G, R) = cv2.split(frame.astype("float"))
+            if rAvg is None or total == 3:
+                rAvg, gAvg, bAvg = R, G, B
+                total = 0
+            else:
+                rAvg = ((total * rAvg) + (1 * R)) / (total + 1.0)
+                gAvg = ((total * gAvg) + (1 * G)) / (total + 1.0)
+                bAvg = ((total * bAvg) + (1 * B)) / (total + 1.0)
+            total += 1
+            frame = cv2.merge([bAvg, gAvg, rAvg]).astype("uint8")
 
+        """
+        Showing only the unprocessed camera frame
+        """
         if part == 0:
             # display the current image
             cv2.imshow("Display", frame)
@@ -120,17 +131,26 @@ def CaptureImages(directory, part=1, tune=False, heuristic=False):
         skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
         frame = cv2.bitwise_and(frame, frame, mask=skinMask)
 
+        # attempting to add a medianblur filter to reduce salt/pepper pixel effects
+        frame = cv2.medianBlur(frame, 5)
+        # and then apply a bilateral filter so the edges are not affected by noise reduction
+        frame = cv2.bilateralFilter(frame, 9, 75, 75) #diameter, sigmaColor for color space, sigmaColor for coordinatespace. larger value for sigma color means colors far apart will mix, and more pixels too.
+
         # Binarize the image
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         threshold_value = cv2.getTrackbarPos('threshold', window_name)
 
+        """
+        This part produces binarized hand image
+        """
         if part == 1:
-            ret, thresh = cv2.threshold(gray, 0, threshold_value, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+            ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
             # display the current image
-            cv2.imshow("Display", frame)
+            cv2.imshow("Display", thresh)
 
-        if part == 2 or part == 4:
-            ret, thresh = cv2.threshold(gray, 0, threshold_value, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+        if part == 2:
+            ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
             # Connected components analysis
             ret, markers, stats, centroids = cv2.connectedComponentsWithStats(thresh, ltype=cv2.CV_16U)
             markers = np.array(markers, dtype=np.uint8)
@@ -147,74 +167,85 @@ def CaptureImages(directory, part=1, tune=False, heuristic=False):
                     x, y, w, h = roi
                     subImg = labeled_img[y:y+h, x:x+w]
                     subImg = cv2.cvtColor(subImg, cv2.COLOR_BGR2GRAY)
-                    _, contours, _ = cv2.findContours(subImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    # _, contours, _ = cv2.findContours(subImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+                    contours, _ = cv2.findContours(subImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
                     maxCntLength = 0
                     for i in range(0, len(contours)):
                         cntLength = len(contours[i])
                         if (cntLength > maxCntLength):
                             cnt = contours[i]
                             maxCntLength = cntLength
-                        if (maxCntLength >= 5):
-                            ellipseParam = cv2.fitEllipse(cnt)
-                            subImg = cv2.cvtColor(subImg, cv2.COLOR_GRAY2RGB)
-                            subImg = cv2.ellipse(subImg, ellipseParam, (0, 255, 0), 2)
-                        
-                        (x, y), (MA, ma), angle = cv2.fitEllipse(cnt)
-                        print("(x, y) = ({}, {}), (MA, ma) = ({}, {}), angle = {}".format(x, y, MA, ma, angle))
+                    if (maxCntLength >= 5):
+                        ellipseParam = cv2.fitEllipse(cnt)
+                        # (x, y), (MA, ma), angle = ellipseParam
+                        # print("(x, y) = ({}, {}), (MA, ma) = ({}, {}), angle = {}".format(x, y, MA, ma, angle))
+                        # text = "angle: " + str(angle)
+                        # cv2.putText(labeled_img, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (209, 80, 0, 255), 3)
+                        # if part == 4:
+                        #     cX = offsetX + scaleX * x
+                        #     cY = offsetY + scaleY * y
+                        #     print(cX, cY)
+                        #     pyautogui.moveTo(cX, cY, duration=0.02, tween=pyautogui.easeInOutQuad)
+                        subImg = cv2.cvtColor(subImg, cv2.COLOR_GRAY2RGB)
+                        subImg = cv2.ellipse(subImg, ellipseParam, (0, 255, 0), 2)
 
-                        if part == 4:
-                            pyautogui.moveTo(cX, cY, duration=0.02, tween=pyautogui.easeInOutQuad)
-                            if fingerCount == 2:
-                                pyautogui.click()
-
-                        subImg = cv2.resize(subImg, (0, 0), fx=3, fy=3)
-                        cv2.imshow("ROI "+str(2), subImg)
-                        cv2.waitKey(1)
+                    subImg = cv2.resize(subImg, (0, 0), fx=3, fy=3)
+                    cv2.imshow("ROI "+str(2), subImg)
+                    cv2.waitKey(1)
                 except:
                     print("No hand found")
 
             # display the current image
             cv2.imshow("Display", labeled_img)
 
-        if part == 3:
+        if part == 3 or part == 4:
             ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-            _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             contours = sorted(contours, key=cv2.contourArea, reverse=True)
             thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+            fingerAngles = []
             if len(contours) > 1:
                 largestContour = contours[0]
                 hull = cv2.convexHull(largestContour, returnPoints=False)
-                for cnt in contours[:1]:
-                    defects = cv2.convexityDefects(cnt, hull)
-                    if (not isinstance(defects, type(None))):
-                        fingerCount = 0
-                        for i in range(defects.shape[0]):
-                            s, e, f, d = defects[i, 0]
-                            start = tuple(cnt[s][0])
-                            end = tuple(cnt[e][0])
-                            far = tuple(cnt[f][0])
+                cv2.drawContours(thresh,[hull],-1,225,3)
+                # for cnt in contours[:1]:
+                #     defects = cv2.convexityDefects(cnt, hull)
+                #     if (not isinstance(defects, type(None))):
+                #         fingerCount = 0
+                #         for i in range(defects.shape[0]):
+                #             s, e, f, d = defects[i, 0]
+                #             start = tuple(cnt[s][0])
+                #             end = tuple(cnt[e][0])
+                #             far = tuple(cnt[f][0])
 
-                            if heuristic:
-                                # Defect check
-                                c_squared = (end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2
-                                a_squared = (far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2
-                                b_squared = (end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2
-                                angle = np.arccos((a_squared + b_squared - c_squared) / (2 * np.sqrt(a_squared * b_squared)))
+                #             if heuristic:
+                #                 # Defect check
+                #                 c_squared = (end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2
+                #                 a_squared = (far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2
+                #                 b_squared = (end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2
+                #                 angle = np.arccos((a_squared + b_squared - c_squared) / (2 * np.sqrt(a_squared * b_squared)))
 
-                                if angle <= np.pi / 3:
-                                    fingerCount += 1
-                                    cv2.line(thresh, start, end, [0, 255, 0], 2)
-                                    cv2.circle(thresh, far, 4, [0, 0, 255], -1)
-                            else:
-                                cv2.line(thresh, start, end, [0, 255, 0], 2)
-                                cv2.circle(thresh, far, 4, [0, 0, 255], -1)
+                #                 if angle <= np.pi / 3:
+                #                     fingerAngles.append(angle)
+                #                     fingerCount += 1
+                #                     cv2.line(thresh, start, end, [0, 255, 0], 2)
+                #                     cv2.circle(thresh, far, 4, [0, 0, 255], -1)
+                #             else:
+                #                 cv2.line(thresh, start, end, [0, 255, 0], 2)
+                #                 cv2.circle(thresh, far, 4, [0, 0, 255], -1)
 
-                        text = "Finger count: " + str(fingerCount + int(fingerCount != 0))
-                        cv2.putText(thresh, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (209, 80, 0, 255), 3)
-                # Print center coordinates and the area of the contour
-                M = cv2.moments(largestContour)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
+                #         text = "Finger count: " + str(fingerCount + int(fingerCount != 0))
+                #         cv2.putText(thresh, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (209, 80, 0, 255), 3)
+                # # Print center coordinates and the area of the contour
+                # if part == 4:
+                #     M = cv2.moments(largestContour)
+                #     cX = offsetX + scaleX * int(M["m10"] / M["m00"])
+                #     cY = offsetY + scaleY * int(M["m01"] / M["m00"])
+                #     pyautogui.moveTo(cX, cY, duration=0.02, tween=pyautogui.easeInOutQuad)
+                #     print(fingerAngles)
+                #     if fingerCount == 2:
+                #         pyautogui.rightClick()
                 # logging.info('Center: ({}, {}), Area: {}'.format(cX, cY, M))
             # display the current image
             cv2.imshow("Display", thresh)
@@ -237,6 +268,7 @@ def get_args():
     parser.add_argument('-p', '--part', metavar='E', type=int, default=1, help='Part of the homework to run', dest='part')
     parser.add_argument('-t', '--tune', metavar='T', type=str, nargs='?', const=True, default=False, help='Use the trackbar to tune parameters', dest='tune')
     parser.add_argument('-hf', '--heuristic-filtering', metavar='H', type=str, nargs='?', const=True, default=False, help='Apply heuristic filtering to defects', dest='heu_filtering')
+    parser.add_argument('-twa', '--time-window-averaging', metavar='A', type=str, nargs='?', const=True, default=False, help='Apply time window averaging', dest='time_window_averaging')
     return parser.parse_args()
 
 # A function that does nothing
@@ -247,4 +279,4 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     args = get_args()
     print(args)
-    CaptureImages("Hand-images", part=args.part, tune=args.tune, heuristic=args.heu_filtering)
+    CaptureImages("Hand-images", part=args.part, tune=args.tune, heuristic=args.heu_filtering, twa=args.time_window_averaging)
